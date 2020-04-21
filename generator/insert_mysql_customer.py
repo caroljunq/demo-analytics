a62b4796c04390a3044f63f49773c0ccd1190715
Correct Name <junqueiracarolina@live.com>
2020-04-21 00:51:23 -0300

    pymysql

--- generator/insert_mysql_customer.py
+++ generator/insert_mysql_customer.py
@@ -10,6 +10,8 @@ import pymysql as mysql
 with open('../config.json') as data:
     config = json.load(data)
 
+language = config["language"]
+
 person = mimesis.Person(language)
 address = mimesis.Address(language)
 
@@ -34,39 +36,34 @@ ages = list(range(min_age,max_age + 1))
 
 n_inserts = int(sys.argv[1])
 
-try:
-    connection = mysql.connect(host='hostname',
-                                        database='database_name',
-                                        user='username',
-                                        password='pass'
-                                )
 
-    mysql_insert_query = """INSERT INTO customers_cdc (customer_id, 
-                            age, country_id, gender, 
-                            city, state) 
-                            VALUES (%s, %s, %s, %s,%s, %s) """
+connection = mysql.connect(host='hostname',
+                            database='database_name',
+                            user='username',
+                            password='pass'
+                        )
 
-    records_to_insert = []
+mysql_insert_query = """INSERT INTO customers_cdc (customer_id, 
+                        age, country_id, gender, 
+                        city, state) 
+                        VALUES (%s, %s, %s, %s,%s, %s) """
 
-    for i in range(n_inserts):
-        customer_id = random.getrandbits(128)
-        country_id = np.random.choice(countries, p=country_prob)
-        age = np.random.choice(ages, p=ages_probab)
-        gender = np.random.choice(genders, p=gender_prob)
-        city = address.city().replace(',','')
-        state = address.state().replace(',','')
-        records_to_insert.add((customer_id,age,country_id,gender,city,state))
+records_to_insert = []
 
-    cursor = connection.cursor()
-    cursor.executemany(mysql_insert_query, records_to_insert)
-    connection.commit()
-    print(cursor.rowcount, "Record inserted successfully into customers_cdc table")
+for i in range(n_inserts):
+    customer_id = random.getrandbits(128)
+    country_id = np.random.choice(countries, p=country_prob)
+    age = np.random.choice(ages, p=ages_probab)
+    gender = np.random.choice(genders, p=gender_prob)
+    city = address.city().replace(',','')
+    state = address.state().replace(',','')
+    records_to_insert.add((customer_id,age,country_id,gender,city,state))
 
-except mysql.connector.Error as error:
-    print("Failed to insert records into MySQL table {}".format(error))
+cursor = connection.cursor()
+cursor.executemany(mysql_insert_query, records_to_insert)
+connection.commit()
+print(cursor.rowcount, "Record inserted successfully into customers_cdc table")
 
-finally:
-    if (connection.is_connected()):
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
+cursor.close()
+connection.close()
+print("MySQL connection is closed")
